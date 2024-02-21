d6ccccdba638c25699d028fa053ccec6fe338633
Daniel Xie <daniel.xie@farfetch.com>
2024-02-21 17:39:55 +0800

    init

---
+++ deploy-commands.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } = process.env;
+const { REST, Routes } = require('discord.js');
+
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing required "data" or "execute" properties.`);
+		}
+	}
+}
+
+const rest = new REST().setToken(DISCORD_TOKEN);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error) {
+		console.error(error);
+	}
+})();
